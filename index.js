6c72c9c988d01c67d9052355d6a8d055b105af20
deif <deif@pm.me>
2022-04-15 14:32:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+module.exports = {
+    root: true,
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "@rushstack/eslint-config/profile/web-app",
+        "plugin:vue/vue3-recommended",
+    ],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "vue/multi-word-component-names": "off",
+    },
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)",
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+}
