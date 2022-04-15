cba1946737554f8a22a218b4a9de8e2ae394b146
deif <deif@pm.me>
2022-04-15 15:08:28 +0100

    add vue eslint

--- index.js
+++ index.js
@@ -11,19 +11,21 @@ module.exports = {
         "@rushstack/eslint-config/profile/web-app",
         "plugin:vue/vue3-recommended",
     ],
-    parserOptions: {
-        project: './tsconfig.json'
-    },
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
         "prettier/prettier": ["error", {}, { usePrettierrc: true }],
-        "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
         "vue/multi-word-component-names": "off",
     },
     overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                project: './tsconfig.json'
+            }
+        },
         {
             files: [
                 "**/__tests__/*.{j,t}s?(x)",
